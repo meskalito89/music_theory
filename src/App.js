87761cf7e2da1aebcfbfa6fa5755cd27a3e8ab86
meskalito89 <rokinea89mone@mail.ru>
2020-09-27 20:01:17 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
+import abcjs from 'abcjs';
 import React from 'react';
-import abcjs from 'abcjs'
+
 
 const CONTROLLER_WIDTH = 200;
-const CIRCLES_COLOR = "#ff0000";
+const CIRCLES_COLOR = "azure";
 const CONTROLLER_r = CONTROLLER_WIDTH/9.7;
 const CONTROLLER_R = CONTROLLER_WIDTH/2 - CONTROLLER_r;
-const ANSWERS = ["C", "G", "D", "A", "E", "B", "F#", "C#", "G#", "D#", "A#", "F"];
-const notes = ["a", "b", ]
+const LABLES = ["C", "G", "D", "A", "E", "B", "F#", "C#", "G#", "D#", "A#", "F"];
+const abc_notes = {
+  "C": ["C,", "C", "c", "c'"],
+  "C#": ["^C,", "^C", "^c", "^c'", "_D,", "_D", "_d", "_d'"],
+  "D": ["D,", "D", "d", "d'"],
+  "D#": ["^D,", "^D", "^d", "^d'", "_E,", "_E", "_e", "_e'"],
+  "E": ["E,", "E", "e", "e'"],
+  "F": ["F,", "F", "f", "f'"],
+  "F#": ["^F,", "^F", "^f", "^f'", "_G,", "_G", "_g", "_g'"],
+  "G": ["G,", "G", "g", "g'"],
+  "G#": ["^G,", "^G", "^g", "^g'", "_A,", "_A", "_a", "_a'"],
+  "A": ["A,", "A", "a", "a'"],
+  "A#": ["^A,", "^A", "^a", "^a'", "_B,", "_B", "_b", "_b'"],
+  "B": ["B,", "B", "b", "b,"],
+}
 
-
-let minded_note = ANSWERS[Math.floor(Math.random()*12)];
+let minded_lable = ""
 let answer = "";
+
+let get_random_abc_note = ()=>{
+  let keys = Object.keys(abc_notes)
+  let random_key = keys[Math.floor(Math.random()*keys.length)]
+  minded_lable = random_key 
+  let notes = abc_notes[random_key]
+  let note = notes[Math.floor(Math.random()*notes.length)]
+  return note
+}
+let minded_note = get_random_abc_note();
+
+let convert_abc_notation_in_label = (notation) =>{
+  let sharp = notation[0]=="^" ? "#": ""
+  return notation[1].toUpperCase() + sharp
+}
+
 
 function get_coords(){
 
@@ -28,7 +57,7 @@ function get_coords(){
 }
 
 function set_answer( answ ){
-  answer =ANSWERS[answ]
+  answer = LABLES[answ]
 }
 
 function draw_circles(){
@@ -48,7 +77,7 @@ function draw_circles(){
 
 function draw_text(){
   let coords = get_coords();
-  return ANSWERS.map((note, i)=><text 
+  return LABLES.map((note, i)=><text 
                                   x={coords[i].x} 
                                   y={coords[i].y} 
                                   textAnchor="middle"
@@ -58,13 +87,12 @@ function draw_text(){
 }
 
 function next_if_right_answer(){
-  if(answer === minded_note){
-    minded_note = notes[Math.floor(Math.random()*notes.length)].toUpperCase()
+  display_notation(minded_note)
+
+  if(minded_lable === answer){
+    minded_note = get_random_abc_note()
+    display_notation(minded_note)
   }
-  else {
-    console.log('wrong')
-  }
-  return minded_note
 }
 
 function draw_center(){
@@ -72,16 +100,19 @@ function draw_center(){
           cx={CONTROLLER_WIDTH/2}
           cy={CONTROLLER_WIDTH/2}
           r={CONTROLLER_R/2}
-          onMouseEnter={()=>next_if_right_answer()}
+          onMouseEnter={()=>next_if_right_answer() }
          ></circle>
 };
 
+function display_notation(notation){
+  abcjs.renderAbc('display', notation)
+}
 
 
 function App() {
   return (
     <div className="App">
-      <Display notation={answer || "C"}></Display>
+      <div id="display"></div>
       <svg id="controller" width={CONTROLLER_WIDTH} height={CONTROLLER_WIDTH}>
         {draw_circles()}
         {draw_text()}
